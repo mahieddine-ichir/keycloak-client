397b1e959ff4fab9b031a015807bc868d2d400fd
Mahieddine ICHIR <mahieddine.ichir@gmail.com>
2018-04-27 11:17:13 +0200

    some css styling

--- src/main/java/net/michir/kk/Application.java
+++ src/main/java/net/michir/kk/Application.java
@@ -125,8 +125,14 @@ public class Application {
 
                 Collection sendings = (Collection) exchange.getBody().get("sendings");
 
-                this.sendingsHtml = "";
-                sendingsHtml += "<body><table style=\"width:100%\">";
+                this.sendingsHtml = "<html lang=\"en\">";
+                this.sendingsHtml += "<head><meta charset=\"utf-8\">\n" +
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">";
+                this.sendingsHtml += "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css\" integrity=\"sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4\" crossorigin=\"anonymous\">\n" +
+                        "</head>";
+
+                sendingsHtml += "<h1>Mes envois</h1>";
+                sendingsHtml += "<body><table class=\"table\">";
                 sendingsHtml += "<tr>";
                 sendingsHtml += "<th>ID</th>";
                 sendingsHtml += "<th>Nom</th>";
@@ -145,7 +151,12 @@ public class Application {
                     sendingsHtml += "</tr>";
 
                 });
-                sendingsHtml += "</table></body>";
+                sendingsHtml += "</table>" +
+                        "<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\n" +
+                        "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js\" integrity=\"sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ\" crossorigin=\"anonymous\"></script>"+
+                        "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js\" integrity=\"sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm\" crossorigin=\"anonymous\"></script>"+
+                        "</body></html>";
+
                 return sendingsHtml;
             }
 
